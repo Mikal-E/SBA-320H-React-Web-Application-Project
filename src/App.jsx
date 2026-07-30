e8f9e534d59d1fa2bca256fdff0189181612820f
Mikal-E <melamin101@gmail.com>
2026-07-29 23:08:22 -0400

    Built out the fetch to test API connection and received data back successfully.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
+import { useState } from "react"
+
 function App() {
+
+const [query, setQuery] = useState("")
+const [articles, setArticles] = useState([])
+
+const fetchNews = async () => {
+
+  const apiKey = import.meta.env.VITE_GNEWS_API_KEY
+  const url = `https://gnews.io/api/v4/search?q=${encodeURIComponent(query)}&lang=en&max=10&apikey=${apiKey}`
+
+    try {
+
+      const response = await fetch(url)
+      const data = await response.json()
+
+      console.log(data)
+
+      setArticles(data.articles || [])
+      
+    } catch (error) {
+
+      console.error("Error fetching news:", error)
+      
+    }
+
+}
 
   return (
 
     <div>
 
       <h1>Pulse<sup>by Brief.io</sup></h1>
+
+      <input
+      
+      type="text"
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      placeholder="Search industry or brand"
+      
+      />
+
+      <button onClick={fetchNews}>Search</button>
+
 
     </div>
 
