f6b17dad8ba7fe5fb8aa5eecb0367308218b9f50
Mikal-E <melamin101@gmail.com>
2026-07-31 11:32:06 -0400

    Built out a Sidebar.jsx component for a logo area, navigation, and sidebar footer per my wireframe.

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,73 @@
+import pulseLogo from "../assets/pulse-logo.png"
+
+function Sidebar() {
+
+    return (
+
+        <aside className="sidebar">
+
+            <div className="sidebar-header">
+
+                <img src={pulseLogo} alt="Pulse logo" className="logo"/>
+                <h1>Pulse<sup>by Brief.io</sup></h1>
+
+            </div>
+
+            <nav className="sidebar-nav">
+
+                <ul>
+
+                    <li className="future-feature">
+
+                        <i class="fa-solid fa-lightbulb"></i>
+
+                    </li>
+
+                    <li className="future-feature">
+
+                        <i class="fa-solid fa-lightbulb"></i>
+
+                    </li>
+
+                    <li className="future-feature">
+
+                        <i class="fa-solid fa-lightbulb"></i>
+
+                    </li>
+
+                    <li className="future-feature">
+
+                        <i class="fa-solid fa-lightbulb"></i>
+
+                    </li>
+
+                    <li className="future-feature">
+
+                        <i class="fa-solid fa-lightbulb"></i>
+
+                    </li>
+
+                    <li className="future-feature">
+
+                        <i class="fa-solid fa-lightbulb"></i>
+
+                    </li>                                                                                                    
+
+                </ul>
+
+            </nav>
+
+            <div className="sidebar-footer">
+
+                <p><i class="fa-solid fa-gear"></i>Settings</p>
+                <p><i class="fa-solid fa-right-from-bracket"></i>Log Out</p>
+
+            </div>
+
+        </aside>
+
+    )
+
+}
+
+export default Sidebar
